a4a04f433aae2e328a65c88998417a3d08c66228
Alex-Stupak <stupakalexx@gmail.com>
2023-08-06 16:09:19 +0300

    dz5

---
+++ controllers/auth.js
@@ -0,0 +1,115 @@
+const bcrypt = require("bcrypt");
+const { User } = require("../models/user");
+const { HttpError } = require("../helpers");
+const { ctrlWrapper } = require("../decorators");
+const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
+
+const { SECRET_KEY } = process.env;
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
+
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw new HttpError(409, "Email in use");
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
+
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
+
+  res.status(201).json({
+    user: {
+      email: newUser.email,
+      subscription: "starter",
+    },
+  });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new HttpError(401, "Email or password is wrong");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw new HttpError(401, "Email or password is wrong", {
+      expiresIn: "24h",
+    });
+  }
+  const payload = {
+    id: user._id,
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+  await User.findByIdAndUpdate(user._id, { token });
+  res.json({
+    token,
+    user: {
+      email,
+      subscription: "starter",
+    },
+  });
+};
+
+const getCurrent = async (req, res) => {
+  const { email, subscription } = req.user;
+  res.json({ email, subscription });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+
+  res.status(204).json();
+};
+
+const subscription = async (req, res) => {
+  const { _id, subscription } = req.user;
+  const result = await User.findByIdAndUpdate(_id, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw new HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+};
+
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const filename = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, filename);
+
+  await Jimp.read(tempUpload)
+    .then((avatar) => avatar.resize(250, 250).write(filename))
+    .catch((err) => console.log(err));
+
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatars", filename);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    avatarURL,
+  });
+};
+
+module.exports = {
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
+  subscription: ctrlWrapper(subscription),
+  updateAvatar: ctrlWrapper(updateAvatar),
+};
---
+++ routes/api/auth.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const auth = require("../../controllers/auth");
+const { validateBody } = require("../../decorators/index");
+const { userJoiSchema } = require("../../schemasJoi/schemasJoi");
+const { authenticate, upload } = require("../../middleware");
+const { validateSubscription } = require("../../decorators");
+const { subscriptionSchema } = require("../../schemasJoi/schemasJoi");
+
+const router = express.Router();
+
+router.post(
+  "/register",
+  validateBody(userJoiSchema.registerSchema),
+  auth.register
+);
+
+router.post("/login", validateBody(userJoiSchema.loginSchema), auth.login);
+
+router.get("/users/current", authenticate, auth.getCurrent);
+
+router.post("/logout", authenticate, auth.logout);
+
+router.patch(
+  "/users",
+  authenticate,
+  validateSubscription(subscriptionSchema),
+  auth.subscription
+);
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  auth.updateAvatar
+);
+module.exports = router;
